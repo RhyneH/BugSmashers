de610e5df05ba86ab5928bc096bef0d46cd65cbd
Rhyne <rhyneallen0628@gmail.com>
2022-03-31 17:15:46 -0400

    templets commit

--- sugahfree/admin.py
+++ sugahfree/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Input
 
+admin.site.register(User)
+admin.site.register(Input)
 # Register your models here.
--- sugahfree/models.py
+++ sugahfree/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    pass
+
+class Input(models.Model):
+    insulin = models.CharField(max_length=50)
+    carbs = models.CharField(max_length=50)
+    food_intake = models.TextField(blank=False)
+
+def __str__(self):
+    return self.input    
 # Create your models here.
