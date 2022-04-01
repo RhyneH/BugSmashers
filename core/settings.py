8004e853208d7f898f10361a4bc7b62f2d031d59
Rhyne <rhyneallen0628@gmail.com>
2022-04-01 17:50:50 -0400

    homepage/registration

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sugahfree',
+    'myapi.apps.MyapiConfig',
+    
 ]
 
 MIDDLEWARE = [
