3dfd20ac064a64a6a160045b80162257d22a2cda
Rhyne <rhyneallen0628@gmail.com>
2022-04-05 17:52:39 -0400

    fixed migration error

--- sugahfree/admin.py
+++ sugahfree/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Input
+from .models import User
+from myapi.models import Input
 
 admin.site.register(User)
 admin.site.register(Input)
--- sugahfree/views.py
+++ sugahfree/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse 
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Input 
+from myapi.models import Input
 
 def index(request):
     input = Input.objects.all()
