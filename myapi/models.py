26f230c305d6244b2a3f4cf88e7159ceb5afd423
Rhyne <rhyneallen0628@gmail.com>
2022-04-07 18:01:28 -0400

    API Finalized

--- myapi/models.py
+++ myapi/models.py
@@ -3,8 +3,8 @@ from django.db import models
 from sugahfree.models import User
 
 class Input(models.Model):
-    insulin = models.CharField(max_length=50)
-    carbs = models.CharField(max_length=50)
+    insulin_in_units = models.CharField(max_length=50)
+    carbs_in_grams = models.CharField(max_length=50)
     food_intake = models.TextField(blank=False)
     user = models.ForeignKey(User, related_name= 'inputs', on_delete= models.CASCADE)
     created = models.DateTimeField(auto_now_add= True)
