15cad41bbc9ac22d2c8e472908a45ba1ec4dea87
laurell <laurellmurray@gmail.com>
2022-04-12 17:01:09 -0400

    homepage and api connected

--- sugahfree/views.py
+++ sugahfree/views.py
@@ -7,20 +7,32 @@ from myapi.models import Input
 from .forms import InputForm
 
 def index(request):
+    user = request.user
     input = Input.objects.all()
     form = InputForm()
-    return render(request, 'sugahfree/home.html')
+    return render(request, 'sugahfree/home.html', {'form': form, 'user': user})
 
-def add_input(request):
-    if request.method == "POST":
-        print(request.POST)
-        form = InputForm(request.POST)
-        if form.is_vaild():
-            new_input = form.save()
-            return redirect("home")
-    else:
-        form = InputForm()
-    return render(request, 'sugahfree/add_input.html', {'form': form})
+# def ajax_add_input(request):
+#     data = {}
+#     if request.method == "POST":
+#         print(request.POST)
+#         insulin_in_units = request.POST.get('insulin_in_units')
+#         carbs_in_grams = request.POST.get('carbs_in_grams')
+#         food_intake = request.POST.get('food_intake')
 
+#         form = InputForm(request.POST)
+
+#         if form.is_vaild():
+#             new_input = form.save()
+#             data['saved'] = True
+#         else:
+#             data['saved'] = False
+#         data['insulin_in_units'] = insulin_in_units
+#         data['carbs_in_grams'] = carbs_in_grams
+#         data['food_intake'] = food_intake
+
+#     else:
+#         data['response': 'nothing to get']
+#     return JsonResponse(data)
 
 
